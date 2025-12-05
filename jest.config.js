bc8e796509717e8bda64364aaac5f01ff474fc93
Vili Sihvo <vili.sihvo@tuni.fi>
2025-12-05 13:05:33 +0200

    Adjusted npm test command to run coverage report with the tests. Coverage report currently only covers the ten files planned for testing. This can be adjusted by editing the jest.config.js file.

---
+++ jest.config.js
@@ -0,0 +1,15 @@
+export default {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/add.js",
+    "src/chunk.js",
+    "src/words.js",
+    "src/slice.js",
+    "src/map.js",
+    "src/isEmpty.js",
+    "src/filter.js",
+    "src/eq.js",
+    "src/toNumber.js",
+    "src/capitalize.js"
+  ]
+};
