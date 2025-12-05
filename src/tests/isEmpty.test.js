3f12ec86985298d7dba3700618e102a94eb19fe3
Vili Sihvo <vili.sihvo@tuni.fi>
2025-12-05 21:50:22 +0200

    Unit tests for isEmpty

--- src/tests/isEmpty.test.js
+++ src/tests/isEmpty.test.js
@@ -1,5 +1,51 @@
 import isEmpty from '../isEmpty.js';
 
-test('Returns correct value (true) when an array is truly empty', () => {
-  expect(isEmpty([])).toBe(true);
+describe('Should return true.', () => {
+  test('Returns true when an array is truly empty', () => {
+    expect(isEmpty([])).toBe(true);
+  });
+
+  test('Returns true when called with null', () => {
+    expect(isEmpty(null)).toBe(true); 
+  });
+
+  test('Returns true when called with an int number', () => {
+    expect(isEmpty(5)).toBe(true);
+  });
+
+  test('Returns true when called with a decimal number', () => {
+    expect(isEmpty(5.12)).toBe(true);
+  });
+
+  test('Returns true when called with an empty set', () => {
+    const set = new Set([]);
+    expect(isEmpty(set)).toBe(true);
+  });
+
+  test('Returns true when called with an empty map', () => {
+    const map = new Map([]);
+    expect(isEmpty(map)).toBe(true);
+  })
+
+  test('Returns true when called with true', () => {
+    expect(isEmpty(true)).toBe(true);
+  })
+});
+
+describe('Should return false.', () => {
+  test('One length string returns false', () => {
+    expect(isEmpty("a")).toBe(false);
+  });
+
+  test('Non-empty object returns false', () => {
+    expect(isEmpty({ 'a': 1 })).toBe(false);
+  });
+
+  test('Array with multiple elements returns false', () => {
+    expect(isEmpty([1,2,3])).toBe(false);
+  })
+
+  test('Returns false when called with false', () => {
+    expect(isEmpty(false)).toBe(true);
+  })
 });
